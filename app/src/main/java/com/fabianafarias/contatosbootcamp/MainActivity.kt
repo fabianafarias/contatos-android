a4dfb55fd47367b93fca0ff4c30d3c87adb9aca7
fabiana farias <farias.fabianam@gmail.com>
2021-05-17 18:22:47 -0300

    Desenvolvendo a classe MainActivity

--- app/src/main/java/com/fabianafarias/contatosbootcamp/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/contatosbootcamp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.fabianafarias.contatosbootcamp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_CONTACT = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
+        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.READ_CONTACTS),
+            REQUEST_CONTACT)
+        }else {
+            setContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray ) {
+        if (requestCode == REQUEST_CONTACT) setContacts()
+    }
+
+    private fun setContacts() {
+        TODO("Not yet implemented")
     }
 }
